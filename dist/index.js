52034998f8c9b01021d0369f6f126451ce108ee7
Subramani V <106590803+subu-v@users.noreply.github.com>
2023-01-07 23:41:40 +0530

    task completed

--- dist/index.js
+++ dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61081" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53473" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
